7f99111987c392b868f69fb63028efb33c8f0f25
Justin Grubbs <grubbsjr@hendrix.edu>
2023-02-16 16:41:39 -0600

    Added Polygon Collider and Basic Animations to Rat

--- BakerScratchZooPropane/Assets/Scripts/Player.cs
+++ BakerScratchZooPropane/Assets/Scripts/Player.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private Rigidbody2D body;
+    public float horizontal;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    public float runSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      horizontal = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("horizontal", horizontal);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print("Space!");
+            body.AddForce(new Vector2(0, runSpeed * 100));
+        }
     }
+
+    void FixedUpdate() {
+        body.velocity = new Vector2(horizontal * runSpeed, body.velocity.y);
+    }
+    
 }
